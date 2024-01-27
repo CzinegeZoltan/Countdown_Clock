cca1f21c34b66d93c26273516c9b625be0168f74
Czinege Zoltán <czinege.zoltan_19c@bolyai-szakkozep.hu>
2024-01-27 12:22:26 +0100

    refakt 2024.01.27 12:22

---
+++ Visszaszámláló_óra/Visszaszámláló_óra/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿
+namespace Visszaszámláló_óra
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.countdownLabel = new System.Windows.Forms.Label();
+            this.hourTextBox = new System.Windows.Forms.TextBox();
+            this.minuteTextBox = new System.Windows.Forms.TextBox();
+            this.secondTextBox = new System.Windows.Forms.TextBox();
+            this.startButton = new System.Windows.Forms.Button();
+            this.restartButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // countdownLabel
+            // 
+            this.countdownLabel.Font = new System.Drawing.Font("LCARS", 200.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.countdownLabel.ForeColor = System.Drawing.Color.White;
+            this.countdownLabel.Location = new System.Drawing.Point(-26, 53);
+            this.countdownLabel.Name = "countdownLabel";
+            this.countdownLabel.Size = new System.Drawing.Size(1126, 257);
+            this.countdownLabel.TabIndex = 0;
+            this.countdownLabel.Text = "00:00:00.00";
+            this.countdownLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.countdownLabel.Click += new System.EventHandler(this.countdownLabel_Click);
+            // 
+            // hourTextBox
+            // 
+            this.hourTextBox.BackColor = System.Drawing.Color.Black;
+            this.hourTextBox.Font = new System.Drawing.Font("LCARS", 200.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hourTextBox.ForeColor = System.Drawing.Color.White;
+            this.hourTextBox.Location = new System.Drawing.Point(102, 50);
+            this.hourTextBox.Name = "hourTextBox";
+            this.hourTextBox.Size = new System.Drawing.Size(184, 310);
+            this.hourTextBox.TabIndex = 1;
+            this.hourTextBox.Text = "00";
+            this.hourTextBox.TextChanged += new System.EventHandler(this.hourTextBox_TextChanged);
+            // 
+            // minuteTextBox
+            // 
+            this.minuteTextBox.BackColor = System.Drawing.Color.Black;
+            this.minuteTextBox.Font = new System.Drawing.Font("LCARS", 200.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minuteTextBox.ForeColor = System.Drawing.Color.White;
+            this.minuteTextBox.Location = new System.Drawing.Point(322, 50);
+            this.minuteTextBox.Name = "minuteTextBox";
+            this.minuteTextBox.Size = new System.Drawing.Size(184, 310);
+            this.minuteTextBox.TabIndex = 2;
+            this.minuteTextBox.Text = "00";
+            // 
+            // secondTextBox
+            // 
+            this.secondTextBox.BackColor = System.Drawing.Color.Black;
+            this.secondTextBox.Font = new System.Drawing.Font("LCARS", 200.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.secondTextBox.ForeColor = System.Drawing.Color.White;
+            this.secondTextBox.Location = new System.Drawing.Point(550, 53);
+            this.secondTextBox.Name = "secondTextBox";
+            this.secondTextBox.Size = new System.Drawing.Size(178, 310);
+            this.secondTextBox.TabIndex = 3;
+            this.secondTextBox.Text = "00";
+            // 
+            // startButton
+            // 
+            this.startButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 50.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.startButton.Location = new System.Drawing.Point(457, 361);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(206, 88);
+            this.startButton.TabIndex = 4;
+            this.startButton.Text = "🞂";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // restartButton
+            // 
+            this.restartButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 50.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.restartButton.Location = new System.Drawing.Point(457, 361);
+            this.restartButton.Name = "restartButton";
+            this.restartButton.Size = new System.Drawing.Size(206, 88);
+            this.restartButton.TabIndex = 5;
+            this.restartButton.Text = "↺";
+            this.restartButton.UseVisualStyleBackColor = true;
+            this.restartButton.Click += new System.EventHandler(this.restartButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label1.Location = new System.Drawing.Point(938, 439);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Minden ablakba írj számot!";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1084, 461);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.restartButton);
+            this.Controls.Add(this.secondTextBox);
+            this.Controls.Add(this.minuteTextBox);
+            this.Controls.Add(this.hourTextBox);
+            this.Controls.Add(this.countdownLabel);
+            this.MaximumSize = new System.Drawing.Size(1100, 500);
+            this.MinimumSize = new System.Drawing.Size(1100, 500);
+            this.Name = "Form1";
+            this.Text = "Alterai visszaszámláló óra";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label countdownLabel;
+        private System.Windows.Forms.TextBox hourTextBox;
+        private System.Windows.Forms.TextBox minuteTextBox;
+        private System.Windows.Forms.TextBox secondTextBox;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button restartButton;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Visszaszámláló_óra/Visszaszámláló_óra/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NAudio.Wave;
+
+namespace Visszaszámláló_óra
+{
+    public partial class Form1 : Form
+    {
+        private bool isMinuteReached = false;
+        private bool isCountdownFinished = false;
+        private Timer countdownTimer;
+        private WaveOutEvent startSoundPlayer;
+        private WaveOutEvent stopSoundPlayer;
+        private WaveOutEvent one_minSoundPlayer;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Inicializáld a startSoundPlayer objektumot
+            startSoundPlayer = new WaveOutEvent();
+            startSoundPlayer.Init(new Mp3FileReader(@"timer-start.mp3"));
+
+            // Inicializáld a stopSoundPlayer objektumot
+            stopSoundPlayer = new WaveOutEvent();
+            stopSoundPlayer.Init(new Mp3FileReader(@"timer-stop.mp3"));
+
+            // Inicializáld a one_minSoundPlayer objektumot
+            one_minSoundPlayer = new WaveOutEvent();
+            one_minSoundPlayer.Init(new Mp3FileReader(@"timer-one_min.mp3"));
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            // Ellenőrizd, hogy az óra, perc és másodperc mezők tartalmaznak-e érvényes értéket
+            int hour, minute, second;
+            if (!int.TryParse(hourTextBox.Text, out hour) ||
+                !int.TryParse(minuteTextBox.Text, out minute) ||
+                !int.TryParse(secondTextBox.Text, out second))
+            {
+                MessageBox.Show("Hibás bemeneti érték!");
+                return;
+            }
+
+            // Ellenőrizd, hogy az értékek a megfelelő tartományban vannak-e
+            if (hour < 0 || hour > 23 ||
+                minute < 0 || minute > 59 ||
+                second < 0 || second > 59)
+            {
+                MessageBox.Show("Hibás időérték!");
+                return;
+            }
+
+            // Számold ki a visszaszámlálás összes milliszecundumát
+            int totalMilliseconds = (hour * 3600 + minute * 60 + second) * 1000;
+
+            // Indítsd el a visszaszámlálást egy időzítő segítségével
+            countdownTimer = new Timer();
+            countdownTimer.Interval = 10; // 10 milliszecundum
+            countdownTimer.Tick += (timerSender, timerArgs) =>
+            {
+                // Csökkentsd a visszaszámláló értékét 16-tal (16 milliszecundummal)
+                totalMilliseconds -= 16;
+
+                // Ellenőrizd, hogy lejárt-e a visszaszámlálás
+                if (totalMilliseconds <= 0)
+                {
+                    countdownTimer.Stop(); // Ha lejárt, állítsd le az időzítőt
+                    isCountdownFinished = true;
+                    //MessageBox.Show("Visszaszámlálás véget ért!");
+
+                    // Játszd le a stop hangot a végén
+                    stopSoundPlayer.Play();
+                }
+
+                // Számold ki az óra, perc, másodperc és századmásodperc értékeit
+                int remainingHours = totalMilliseconds / (3600 * 1000);
+                int remainingMinutes = (totalMilliseconds % (3600 * 1000)) / (60 * 1000);
+                int remainingSeconds = (totalMilliseconds % (60 * 1000)) / 1000;
+                int remainingMilliseconds = totalMilliseconds % 1000 / 10;
+
+                // Frissítsd a Label szövegét a visszaszámláló értékével
+                if (remainingHours > 0)
+                {
+                    countdownLabel.Text = $"{remainingHours:D2}:{remainingMinutes:D2}:{remainingSeconds:D2}.{remainingMilliseconds:D2}";
+                }
+                else if (remainingMinutes > 0)
+                {
+                    countdownLabel.Text = $"{remainingMinutes:D2}:{remainingSeconds:D2}.{remainingMilliseconds:D2}";
+                }
+                else
+                {
+                    if (remainingSeconds == 0 && remainingMilliseconds == 0)
+                    {
+                        countdownLabel.Text = "00.00";
+                        countdownLabel.TextAlign = ContentAlignment.MiddleCenter; // Középre igazítás
+                    }
+                    else
+                    {
+                        if (remainingSeconds == 0)
+                        {
+                            countdownLabel.Text = $"{remainingMilliseconds:D2}";
+                        }
+                        else
+                        {
+                            countdownLabel.Text = $"{remainingSeconds:D2}.{remainingMilliseconds:D2}";
+                        }
+                        countdownLabel.TextAlign = ContentAlignment.MiddleCenter; // Középre igazítás
+                    }
+                }
+
+                // Ellenőrizd, hogy elérted-e az egy percet
+                if (!isMinuteReached && totalMilliseconds <= 60000)
+                {
+                    countdownLabel.ForeColor = Color.Red; // Piros szín
+                    isMinuteReached = true;
+                    one_minSoundPlayer.Play(); // Játszd le a stop hangot a perc végén
+                    one_minSoundPlayer = new WaveOutEvent();
+                    one_minSoundPlayer.Init(new BlockAlignReductionStream(WaveFormatConversionStream.CreatePcmStream(new Mp3FileReader(@"timer-one_min.mp3"))));
+                }
+
+                // Ellenőrizd, hogy a visszaszámlálás véget ért-e
+                if (isCountdownFinished && totalMilliseconds <= 0)
+                {
+                    countdownTimer.Stop(); // Ha véget ért, állítsd le az időzítőt
+                    //MessageBox.Show("Visszaszámlálás véget ért!");
+                    countdownLabel.Text = "00";
+
+                    // Játszd le a stop hangot a végén
+                    stopSoundPlayer.Play();
+                }
+            };
+
+            // Játszd le a start hangot
+            startSoundPlayer.Play();
+            countdownTimer.Start(); // Indítsd el az időzítőt
+            hourTextBox.Visible = false;
+            minuteTextBox.Visible = false;
+            secondTextBox.Visible = false;
+            startButton.Visible = false;
+            restartButton.Visible = true;
+            label1.Visible = false;
+        }
+
+        private void restartButton_Click(object sender, EventArgs e)
+        {
+            countdownTimer.Stop(); // Állítsd le az időzítőt
+            countdownLabel.Text = "00:00:00.00"; // Alaphelyzetbe állítja a szöveget
+            countdownLabel.ForeColor = Color.White; // Alapértelmezett fehér szín
+            countdownLabel.TextAlign = ContentAlignment.MiddleCenter; // Alapértelmezett igazítás
+            startSoundPlayer.Stop(); // Állítsd le a start hang lejátszását (ha éppen lejátszódik)
+            stopSoundPlayer.Stop(); // Állítsd le a stop hang lejátszását (ha éppen lejátszódik)
+            one_minSoundPlayer.Stop(); // Állítsd le a one_min hang lejátszását (ha éppen lejátszódik)
+            stopSoundPlayer.Init(new BlockAlignReductionStream(WaveFormatConversionStream.CreatePcmStream(new Mp3FileReader(@"timer-stop.mp3"))));
+            startSoundPlayer.Init(new BlockAlignReductionStream(WaveFormatConversionStream.CreatePcmStream(new Mp3FileReader(@"timer-start.mp3"))));
+            one_minSoundPlayer.Init(new BlockAlignReductionStream(WaveFormatConversionStream.CreatePcmStream(new Mp3FileReader(@"timer-one_min.mp3"))));
+            hourTextBox.Visible = true;//óra beirása
+            minuteTextBox.Visible = true;//perc beírása
+            secondTextBox.Visible = true;//másodperc beírása
+            startButton.Visible = true;//start gomb láthatósága
+            restartButton.Visible = false;//reset gomb láthatósága
+            label1.Visible = true;//kis szöveg láthatósága
+            isMinuteReached = false;
+            isCountdownFinished = false;
+        }
+
+        private void countdownLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void hourTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
